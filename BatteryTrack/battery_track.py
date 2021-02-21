9d994be00de4f163f6bc106a45c86d2abaec930d
cpd <ciaran.dunn@gmail.com>
2021-02-21 02:30:58 +0000

    Add skel of battery track

---
+++ BatteryTrack/battery_track.py
@@ -0,0 +1,20 @@
+# Read the battery status and store in a data store for history
+
+# /sys/class/power_supply//BATX/
+
+class battery_info:
+    pass
+
+    def __init__(self):
+        pass
+
+    def read_battery_info(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
