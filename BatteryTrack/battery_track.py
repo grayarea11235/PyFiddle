8cff685898635276f104a51445104de43869fde0
cpd <ciaran.dunn@gmail.com>
2021-02-23 17:24:09 +0000

    Battery

--- BatteryTrack/battery_track.py
+++ BatteryTrack/battery_track.py
@@ -1,19 +1,36 @@
+"""Battery tracking script."""
 # Read the battery status and store in a data store for history
+
+import sqlite3
 
 # /sys/class/power_supply//BATX/
 
-class battery_info:
-    pass
 
-    def __init__(self):
-        pass
+def store_record():
+    """Store the data record in datastore"""
 
-    def read_battery_info(self):
-        pass
+def read_battery_info():
+    """
+    Read the battery status from the linux only system.
+
+    Returns:
+        a dictionary of the information.
+    """
+    with open('/sys/class/power_supply//BATX/capacity', 'r') as in_file:
+        capacity = in_file.readline().rstrip()
+
+    with open('/sys/class/power_supply//BATX/status', 'r') as in_file:
+        status = in_file.readline().rstrip()
+
+    return {
+        'capacity': capacity,
+        'status': status
+    }
 
 
 def main():
-    pass
+    """Start the script."""
+    print(str(read_battery_info()))
 
 
 if __name__ == '__main__':
