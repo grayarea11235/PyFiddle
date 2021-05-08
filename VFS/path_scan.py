0460a79ed4c8dc79bd73476266f628d6a752fd71
grayarea11235 <ciaran.dunn@gmail.com>
2021-05-08 20:57:50 +0100

    check

--- VFS/path_scan.py
+++ VFS/path_scan.py
@@ -1,11 +1,20 @@
 import os
+import sqlite3
+from sqlite3 import Error
+
+def create_datastore(name):
+    pass
+
 
 def path_scan(path):
     print('Scanning... {path}'.format(path=path))
 
     dir_scan = os.scandir(path)
     for entry in dir_scan:
-        print(entry.name)
+        if entry.is_file():
+            print(entry.name + '  ' + str(entry.stat().st_size))
+    
+    dir_scan.close()
 
 
 if __name__ == "__main__":
