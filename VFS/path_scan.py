24e6c3ac63eebf84758b89457dc4eefb8801fab0
grayarea11235 <ciaran.dunn@gmail.com>
2021-05-09 16:35:43 +0100

    CHeck

--- VFS/path_scan.py
+++ VFS/path_scan.py
@@ -3,31 +3,37 @@ import sqlite3
 from sqlite3 import Error
 
 def create_database(name):
-    conn = None
+
+    if not os.path.isfile(db_name):
+        conn = None
     
-    try:
-        conn = sqlite3.connect(name)
-        print(sqlite3.version)
+        try:
+            conn = sqlite3.connect(name)
+            print(sqlite3.version)
 
-        sql_files_table = """ CREATE TABLE IF NOT EXISTS files (
-                                        id integer PRIMARY KEY,
-                                        full_path text NOT NULL,
-                                        create_time int,
-                                        modified_time int,
-                                        access_time int,
-                                        file_size int
-                                    ); """
+            sql_files_table = """ CREATE TABLE IF NOT EXISTS files (
+                                            id integer PRIMARY KEY,
+                                            full_path text NOT NULL,
+                                            create_time int,
+                                            modified_time int,
+                                            access_time int,
+                                            file_size int
+                                        ); """
 
-        conn.execute(sql_files_table)
+            conn.execute(sql_files_table)
 
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+        except Error as e:
+            print(e)
+        finally:
+            if conn:
+                conn.close()
 
 
-def add_entry(conn, entry):
+def file_entry_exisits(conn):
+    pass
+
+
+def add_entry(db_name, conn, entry):
     """
     Create a new file record
     :param conn:
@@ -35,7 +41,7 @@ def add_entry(conn, entry):
     :return: project id
     """
 
-    insert_data = (entry.name, entry.stat().st_ctime, entry.stat().st_mtime, entry.stat().st_atime, entry.stat().st_size)
+    insert_data = (entry.path, entry.stat().st_ctime, entry.stat().st_mtime, entry.stat().st_atime, entry.stat().st_size)
 
     sql = ''' INSERT INTO files(full_path, create_time, modified_time, access_time, file_size)
               VALUES(?, ?, ?, ?, ?) '''
@@ -44,15 +50,15 @@ def add_entry(conn, entry):
     conn.commit()
 
 
-def path_scan(path):
+def path_scan(db_name, path):
     print('Scanning... {path}'.format(path=path))
     
-    conn = sqlite3.connect('./file_data.db')
+    conn = sqlite3.connect(db_name)
 
     dir_scan = os.scandir(path)
     for entry in dir_scan:
 
-        add_entry(conn, entry)
+        add_entry(db_name, conn, entry)
 
         if entry.is_file():
             print(entry.name + '  ' + str(entry.stat().st_size))
@@ -61,7 +67,8 @@ def path_scan(path):
 
 
 if __name__ == "__main__":
-#    create_database('./file_data.db')
+    db_name = './file_data.db'
+    create_database(db_name)
 
-    path_scan("/media/cpd/USB2")
+    path_scan(db_name, "/media/cpd/USB2")
 
