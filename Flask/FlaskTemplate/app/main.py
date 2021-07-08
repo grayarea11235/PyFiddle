e82b41f28e17d3084e60879b1ca8630e283a8179
cpd <ciaran.dunn@gmail.com>
2021-07-09 00:22:40 +0100

    Stuff

--- Flask/FlaskTemplate/app/main.py
+++ Flask/FlaskTemplate/app/main.py
@@ -1,4 +1,6 @@
 import sys
+import platform
+
 
 import flask
 from flask import Flask
@@ -7,13 +9,30 @@ from flask import render_template
 
 app = Flask(__name__)
 
+class AppSystemInfo:
+    def __init__(self, app):
+        self.properties = dict()
+        self.properties['python_executable'] = sys.executable
+        self.properties['flask_version'] = flask.__version__
+        self.properties['import_name'] = app.import_name
+        self.properties['static_url_path'] = app.static_url_path 
+        self.properties['static_folder'] = app.static_folder
+        self.properties['template_folder'] = app.template_folder
+        self.properties['instance_path'] = app.instance_path
+        #self.properties['instance_relative_config'] = app.instance_relative_config
+        self.properties['root_path'] = app.root_path
+
+        self.properties['platform_machine'] = platform.machine()
+        self.properties['platform_version'] = platform.version()
+        self.properties['platform_node'] = platform.node()
+        self.properties['platform_processor'] = platform.processor()
+
+
 @app.route('/info')
 def flask_info():
-    print('In flask_info')
-    ex = sys.executable  # Python executable
-    flask_version = flask.__version__
+    system_info = AppSystemInfo(app)
 
-    return render_template('info.html', executable=ex, flask_version=flask_version)
+    return render_template('info.html', system_info=system_info)
 
 
 @app.route('/')
