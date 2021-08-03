f0168c611eae1b9ec61481f5e8f913f1f7ad7774
cpd <ciaran.dunn@gmail.com>
2021-08-03 22:00:16 +0100

    Stuff

--- Pillow/ex1.py
+++ Pillow/ex1.py
@@ -1,5 +1,18 @@
 from PIL import Image
 
+def double_image_nn():
+    input_image = Image.open('./images/NeptuneGrayScale.tif')
+    
+    input_size_x = input_image.size[0]
+    input_size_y = input_image.size[1]
+
+    print('Input image size x = {} y = {}'.format(input_size_x, input_size_y))
+    output_image = Image.new('L', input_image.size)
+    for x in range(input_size_x):
+        for y in range(input_size_y):
+            v = input_image.getpixel((x, y))
+            print('{},{} = {}'.format(x, y, v))
+
 
 def load_image():
     im = Image.open('./images/NeptuneGrayScale.tif')
@@ -12,8 +25,15 @@ def load_image():
     im.show()
 
 
+def test_image_create():
+    img = Image.new('RGB', (60, 30), color = 'red')
+    img.save('pil_red.png')
+
+
 def main():
-    load_image()
+    double_image_nn()
+    #test_image_create()
+    #load_image()
 
 
 if __name__ == '__main__':
