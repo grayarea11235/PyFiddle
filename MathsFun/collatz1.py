53321a666398ab984b376a43004849c8ca0c9a26
cpd <ciaran.dunn@gmail.com>
2021-08-06 23:43:27 +0100

    Stuff

--- MathsFun/collatz1.py
+++ MathsFun/collatz1.py
@@ -1,17 +1,16 @@
+"""Simple recursive implementation of the collatz conjecture."""
 import csv
 
 
 def collatz(n, res):
-
     res.append(n)
-    #print(n)
 
     if n == 1:
         return
 
-    if (n % 2) == 0: # Even
+    if (n % 2) == 0:  # Even
         collatz(n // 2, res)
-    else:
+    else:             # Odd
         collatz(3*n + 1, res)
 
 
@@ -19,10 +18,10 @@ def main():
     with open('output.csv', 'w', newline='\n')as csvfile:  
         csvwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        res = [] 
+        res = []
 
-        for n in range(1000):
-            collatz(n+1, res)
+        for number in range(1, 1000):
+            collatz(number, res)
             print(res)
             csvwriter.writerow(res)
             res = []
