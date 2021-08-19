599f0124a54f414e82549fe0e8ce9ca444296359
cpd <ciaran.dunn@gmail.com>
2021-08-19 01:15:49 +0100

    Stuff

--- objexp/gui.py
+++ objexp/gui.py
@@ -6,17 +6,23 @@ class MyTree(wx.TreeCtrl):
     def __init__(self, parent, id, position, size, style):
         wx.TreeCtrl.__init__(self, parent, id, position, size, style)
 
+        self.refresh_tree()
+    
+
+    def refresh_tree(self):
+        self.DeleteAllItems()
+
         root = self.AddRoot('Stuff')
         #self.SetPyData(root, ('key', 'value'))
-        mods = self.AppendItem(root, 'Modules')
+        self.mods = self.AppendItem(root, 'Modules')
 
         modules = os.sys.modules
         for module in modules:
-            self.AppendItem(mods, module)
+            new_item = self.AppendItem(self.mods, module)
 
-
-    def refresh_tree(self):
-        pass
+            dir_result = dir(module)
+            for dir_entry in dir_result:
+                self.AppendItem(new_item, dir_entry)
 
 
 class MainApp(wx.Frame):
@@ -29,6 +35,7 @@ class MainApp(wx.Frame):
     def initUI(self):
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
+        #fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
         fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
         menubar.Append(fileMenu, '&File')
         self.SetMenuBar(menubar)
@@ -40,12 +47,13 @@ class MainApp(wx.Frame):
 
         # Create the left panel
         leftPanel = wx.Panel(self.splitter, -1)
+        
         # Create a box sizer that will contain the left panel contents
         leftBox = wx.BoxSizer(wx.VERTICAL)
 
         # Create our tree and put it into the left panel
         self.tree = MyTree(leftPanel, 1, wx.DefaultPosition, (-1, -1),
-                wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS)
+                wx.TR_HIDE_ROOT | wx.TR_HAS_BUTTONS)
 
         # Add the tree to the box sizer
         leftBox.Add(self.tree, 1, wx.EXPAND)
@@ -58,14 +66,17 @@ class MainApp(wx.Frame):
 
         # Create the right panel
         rightPanel = wx.Panel(self.splitter, -1)
+
         # Create the right box sizer that will contain the panel's contents
         rightBox = wx.BoxSizer(wx.VERTICAL)
+        
         # Create a widget to display static text and store it in the right
         # panel
         self.display = wx.StaticText(rightPanel, -1, '', (10, 10),
-                                     style=wx.ALIGN_CENTRE)
+                style=wx.ALIGN_LEFT)
         # Add the display widget to the right panel
         rightBox.Add(self.display, -1, wx.EXPAND)
+
         # Set the size of the right panel to that required by the
         # display widget
         rightPanel.SetSizer(rightBox)
@@ -80,19 +91,18 @@ class MainApp(wx.Frame):
 
 
     def OnSelChanged(self, event):
-        '''Method called when selected item is changed
-        '''
+        '''Method called when selected item is changed.'''
         # Get the selected item object
         item =  event.GetItem()
         item_text = self.tree.GetItemText(item)
 
-        d = dir(item_text)
-        document = item_text.__doc__
+        mod = __import__(item_text)
+        document = mod.__doc__
 
-        print(document)
+        #print(document)
 
         # Display the selected item text in the text widget
-        self.display.SetLabel(item_text)
+        self.display.SetLabel(document)
 
 
     def OnQuit(self, e):
