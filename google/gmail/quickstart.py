02271e2725123dcef5712dbc46d333bc1a3a8654
cpd <ciaran.dunn@gmail.com>
2021-09-10 19:17:48 +0100

    gmail testing

--- google/gmail/quickstart.py
+++ google/gmail/quickstart.py
@@ -1,19 +1,95 @@
-from __future__ import print_function
+#from __future__ import print_function
+
 import os.path
+import base64
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
+from email.mime.text import MIMEText
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
 
+    Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+    Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+
+def create_message_with_attachment(
+    sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+    Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+        file: The path to the file to be attached.
+
+    Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    
+    main_type, sub_type = content_type.split('/', 1)
+    
+    if main_type == 'text':
+        fp = open(file, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    
+    filename = os.path.basename(file)
+    
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+
+def authenticate():
     creds = None
 
     # The file token.json stores the user's access and refresh tokens, and is
@@ -34,6 +110,10 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+    return creds
+
+
+def gmail_list_labels(creds):
     service = build('gmail', 'v1', credentials=creds)
 
     # Call the Gmail API
@@ -47,6 +127,11 @@ def main():
         for label in labels:
             print(label['name'])
 
+
+def main():
+    creds = authenticate()
+    gmail_list_labels(creds)
+ 
 
 if __name__ == '__main__':
     main()
