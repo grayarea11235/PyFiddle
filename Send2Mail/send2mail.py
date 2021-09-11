9b10901b329f4537fa6d5da5dd27971807befbb8
cpd <ciaran.dunn@gmail.com>
2021-09-11 18:15:22 +0100

    send2email can send files

--- Send2Mail/send2mail.py
+++ Send2Mail/send2mail.py
@@ -1,13 +1,16 @@
 # Utility to send notes, urls, files etc to a email.
 # Just using SMTP
 
+import os
 import argparse
 import ssl
 import smtplib
 import config
 import base64
+import mimetypes
 
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # Command format
 #   send2mail -f <file>
@@ -38,9 +41,68 @@ def create_message(sender, to, subject, message_text):
     return message
 
 
+def create_message_with_attachment(
+    sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+    Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+        file: The path to the file to be attached.
+
+    Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    print(content_type)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    
+    main_type, sub_type = content_type.split('/', 1)
+    
+    if main_type == 'text':
+        fp = open(file, 'rt')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    
+    filename = os.path.basename(file)
+    
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    #return {'raw': base64.urlsafe_b64encode(message.as_string().as_bytes()).decode()}
+    return message
+
+
 def process_cmd_line():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--string", help="send sgtring to email")
+    parser.add_argument("-s", "--string", help="send string to email")
+    parser.add_argument("-f", "--file", help="send file to email")
 
     args = parser.parse_args()
 
@@ -50,19 +112,21 @@ def process_cmd_line():
     if args.string != None:
         type = 'string'
         data = args.string
+    if args.file != None:
+        type = 'file'
+        data = args.file
 
     return type, data
 
 
 def send_email(msg):
     context = ssl.create_default_context()
-    print(context)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", config.port, context=context) as server:
         server.login(config.user_name, config.password)
         print(server)
-        server.sendmail(config.user_name, config.user_name, msg=msg.as_string())
 
+        server.sendmail(config.user_name, config.user_name, msg=msg.as_string())
 
 def main():
     print(config.user_name)
@@ -73,6 +137,12 @@ def main():
     if type == 'string':
         msg = create_message('ciaran.dunn@gmail.com', 'ciaran.dunn@gmail.com', 'String message from send2email', data)
         print(msg)
+    if type == 'file':
+        print('Sending file! {}'.format(data))
+        msg = create_message_with_attachment(
+            'ciaran.dunn@gmail.com', 'ciaran.dunn@gmail.com', 
+            'File message from send2mail. Name is '.format(data), 'Find attached a file named'.format(data), 
+            data)
     
     if msg:
         send_email(msg)
