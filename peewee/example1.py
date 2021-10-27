6a5828ccd80d17894487e6e5cf5ff12801b667cd
cpd <ciaran.dunn@gmail.com>
2021-10-27 14:42:38 +0100

    Peewee examples

--- peewee/example1.py
+++ peewee/example1.py
@@ -1,4 +1,5 @@
 from peewee import *
+from datetime import date
 
 db = SqliteDatabase('people.db')
 
@@ -8,3 +9,11 @@ class Person(Model):
 
     class Meta:
         database = db # This model uses the "people.db" database.
+
+
+db.connect()
+
+db.create_tables([Person])
+
+test1 = Person(name='Dave', birthday=date(1978, 1, 1))
+test1.save()
