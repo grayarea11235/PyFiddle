730e2b6af34f4c582e30b22ed9557a1d2fc239f8
cpd <ciaran.dunn@gmail.com>
2022-02-15 21:30:01 +0000

    Stuff

--- Audio/mutagen_test.py
+++ Audio/mutagen_test.py
@@ -1,4 +1,7 @@
 from mutagen import File
+from mutagen.mp3 import MP3
+from mutagen.mp4 import MP4
+
 from os import path
 from os import walk
 
@@ -22,6 +25,25 @@ TEST_DIR = '/media/cpd/8067-3833/Audiobooks/'
 TEST_FILE = 'Richard P. Feynman - The Pleasure of Finding Things Out Audiobook.mp3'
 
 
+def print_MPEGInfo(info):
+    return info.pprint()
+#    print('Length      = {length}'.format(length=info.length))
+#    print('Channels    = {channels}'.format(channels=info.channels))
+#    print('Bit Rate    = {bitrate}'.format(bitrate=info.bitrate))
+#    print('Sampel Rate = {sample_rate}'.format(sample_rate=info.sample_rate))
+#encoder_info          
+#encoder_settings
+#bitrate_mode
+#track_gain
+#track_peak
+#album_gain
+#version
+#layer
+#mode
+#protected
+#sketchy
+
+
 def scan_dir(start_path):
     db.connect()
     db.create_tables([AudioFile])
@@ -36,8 +58,17 @@ def scan_dir(start_path):
                 print('Got exception')
             if f is not None:
                 file_name = path.join(root, file) + ' : ' + f.info.pprint()
-                print(file_name)
+                print(file)
                 new_record = AudioFile(file_name = file_name)
+
+                if isinstance(f, MP3):
+                    print('FOUND AN MP3 : {info}'.format(info=print_MPEGInfo(f.info)))
+                elif isinstance(f, MP4):
+                    print('FOUND AN MP4 : {info}'.format(info=f.info.pprint()))
+                else:
+                    print(type(f))
+
+                
                 new_record.save()
 
 
