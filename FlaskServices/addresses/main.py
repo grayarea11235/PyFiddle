cd823bf3918e56025d63ddc69c0cce792cef9c4b
cpd <ciaran.dunn@gmail.com>
2023-05-31 19:47:44 +0100

    Added addresses

---
+++ FlaskServices/addresses/main.py
@@ -0,0 +1,28 @@
+# Simple user service
+import logging
+from sqlalchemy import Column, Integer, String
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+
+    id = Column(Integer, primary_key=True)
+    name_or_number = Column(String)
+
+    address1 = Column(String)
+    address2 = Column(String)
+    address3 = Column(String)
+    address4 = Column(String)
+
+    post_code = Column(String)
+
+   def __repr__(self):
+        return "<Address (name_or_number='%s', address1='%s', address2='%s')>" % (
+            self.name_or_number, self.address1, self.address2)
+
+def main():
+    print('In main...')
+
+    
+if __name__ == '__main__':
+    main()
