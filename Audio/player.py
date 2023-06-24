bddd6cb4a909a42994b04f0d9eabb013047dd0dd
cpd <ciaran.dunn@gmail.com>
2023-06-24 20:36:41 +0100

    thread example

--- Audio/player.py
+++ Audio/player.py
@@ -1,4 +1,4 @@
-import sys
+import sys, threading, time, logging
 import gi
 
 gi.require_version('Gst', '1.0')
@@ -31,9 +31,9 @@ class Player(object):
         for i in dir(o):
             #if callable(i) == True:
             print('\t' + i)
-            a = getattr(o, 'getter')
-            print('***** ' + str(type(a("test"))))
-            print(type(a))
+            a = getattr(o, i)
+            #print('***** ' + str(type(a("test"))))
+            #print(type(a))
             print('\t\t' + str(callable(a)) + ' ' + str(a))
         #print(dir_o)
 
@@ -44,15 +44,15 @@ class Player(object):
         print(msg_type)
 
         if msg_type == Gst.MessageType.EOS:
-            print(Gst.Message.timestamp)
-            print(Gst.Message.src)
+            #print(Gst.Message.timestamp)
+            #print(Gst.Message.src)
 
             print('EOS!!!')
 
         if msg_type == Gst.MessageType.INFO:
             print('INFO got')
-            print(Gst.Message.timestamp)
-            print(Gst.Message.src)
+            #print(Gst.Message.timestamp)
+            #print(Gst.Message.src)
 
         if msg_type == Gst.MessageType.TAG:
             self._dump_obj(Gst.Message.timestamp)
@@ -64,12 +64,28 @@ class Player(object):
 
         
     def play(self):
+        #self.play_thread_id = thread.start_new_thread(self.play_thread, ())
+        x = threading.Thread(target=self.play_thread, args=(1,))
+        print(x)
+        x.start()
+
         ret = self._player.set_state(Gst.State.PLAYING)
         if ret == Gst.StateChangeReturn.FAILURE:
             print("ERROR: Unable to set the pipeline to the playing state")
             sys.exit(1)
 
-            
+
+    def play_thread(self):
+        print('Inside thread')
+        play_thread_id = self.play_thread_id
+        Gdk.threads_enter()
+        print('In play_thread')
+        time.sleep(10)
+        print('Done thread')
+#        self.time_label.set_text("00:00 / 00:00")
+        Gdk.threads_leave()
+
+
     def pause(self):
         self._player.set_state(Gst.State.PAUSED)
 
@@ -82,8 +98,13 @@ def init_thread():
     pass
 
 
-
 if __name__ == '__main__':
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    logging.info('ENTER Test')
+
+    
     p = Player()
 #    p.set_file('/home/cpd/Dev/Python/PyFiddle/Audio/Sherlock_06_The Sign of Four.mp3')
     p.set_file('/home/cpd/Devel/PyFiddle/Audio/mpthreetest.mp3')
